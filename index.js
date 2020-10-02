40b2ca7d17252f5c49752cede448017bf64f8950
Casey Brown <casey.92.brown@gmail.com>
2020-10-02 14:00:52 -0700

    refactor: Made message a direct reply rather than global message

--- index.js
+++ index.js
@@ -6,8 +6,25 @@ const prefix = "$";
 
 // inside a command, event listener, etc.
 const pongEmbeded = new Discord.MessageEmbed()
-	.setColor('#0099ff')
+  .setColor('#0099ff')
+  .setDescription('Description goes here')
   .setTitle('Pong!');
+
+const pvpDirectReplyEmbeded = new Discord.MessageEmbed()
+	.setColor('#ff0000')
+  .setTitle('Welcome to PvP!')
+  .addFields(
+    { name: "Events", value: "To see all PvP, go to the 'events' text channel" },
+    { name: "What can I do?", value: "Use the @PvP to trigger a message to other PvP minded individuals" }
+  );
+
+const pveDirectReplyEmbeded = new Discord.MessageEmbed()
+	.setColor('#005dff')
+  .setTitle('Welcome to PvE!')
+  .addFields(
+    { name: "Events", value: "To see all PvE, go to the 'events' text channel" },
+    { name: "What can I do?", value: "Use the @PvE to trigger a message to other PvE minded individuals" }
+  );
 
 const rankEmbeded = new Discord.MessageEmbed()
   .setColor('#00ffe8')
@@ -39,8 +56,6 @@ client.on("message", function(message) {
   }
 
   if(command == "rank"){
-
-    
     message.reply(rankEmbeded).then(sentMessage => {
       sentMessage.react("🔴");
       sentMessage.react("🔵");
@@ -49,8 +64,9 @@ client.on("message", function(message) {
       const collector = sentMessage.createReactionCollector(rankFilter, { time: 30000 });
 
       collector.on('collect', (reaction, user) => {
-        const userReactions = sentMessage.reactions.cache.filter(reaction => reaction.users.cache.has(user.id));
 
+        // clear the users reaction
+        const userReactions = sentMessage.reactions.cache.filter(reaction => reaction.users.cache.has(user.id));
         try {
           for (const reaction of userReactions.values()) {
             reaction.users.remove(user.id);
@@ -58,7 +74,22 @@ client.on("message", function(message) {
         } catch (error) {
           console.error('Failed to remove reactions.');
         }
-        message.channel.send(`${user}, You've selected ${reaction.emoji.name}!`);
+
+        // reply to the user their selection
+        switch(reaction.emoji.name){
+          case "🔴":
+            user.send(pvpDirectReplyEmbeded);
+            break;
+          case "🔵":
+            user.send(pveDirectReplyEmbeded);
+            break;
+          case "❌":
+            // implement
+            break;
+          default:
+            break;
+        }
+        
       });
 
     }).catch(err => {
@@ -66,6 +97,15 @@ client.on("message", function(message) {
     });
     
   }
+
+  if(command == "dev-show-embeds"){
+    message.reply(pongEmbeded);
+    message.reply(pvpDirectReplyEmbeded);
+    message.reply(pveDirectReplyEmbeded);
+    message.reply(rankEmbeded);
+  }
 });
+
+
 
 client.login(config.token);
