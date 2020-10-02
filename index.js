a10b7de18313d417ff169fdf162ffec2c98e9398
James <jwrice1188@gmail.com>
2020-10-02 15:05:09 -0500

    Create embeded message and reactions

--- index.js
+++ index.js
@@ -5,4 +5,20 @@ const client = new Discord.Client();
 
 client.login(config.token);
 
-//insert logic here
+const roleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Choose your preferred play style.')
+	.setDescription('Select the appropriate reaction to add a identifier to your name in this server and the associated role.')
+	.addFields(
+		{ name: 'PvP (Player vs. Player)' , value: '🔴', inline: true },
+		{ name: 'PvE (Player vs. Environment)', value: '🔵', inline: true },
+	)
+
+client.on('message', message => {
+  if (message.content === '!start') {
+    message.channel.send(roleEmbed).then(sentMessage => {
+        sentMessage.react('🔴');
+        sentMessage.react('🔵');
+    });
+  }
+});
